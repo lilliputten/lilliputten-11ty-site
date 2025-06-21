dd53740cd2507643431e4ecacfd675ae7e467011
lilliputten <lilliputten@gmail.com>
2025-06-21 04:18:28 +0300

    Updated maintenance scripts and github workflow actions.

--- .utils/github-publish.sh
+++ .utils/github-publish.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# @desc Create/update version tag (from build folder)
-# @changed 2024.12.11, 03:49
+# @desc Publish (and make if absent) publish build
+# @changed 2025.06.21, 04:22
 
 scriptsPath=$(dirname "$(echo "$0" | sed -e 's,\\,/,g')")
 rootPath=`dirname "$scriptsPath"`
@@ -8,7 +8,6 @@ prjPath="$rootPath" # `pwd`
 
 # Import config variables (expected variable `$PUBLISH_FOLDER`)...
 test -f "$scriptsPath/config.sh" && . "$scriptsPath/config.sh"
-test -f "$scriptsPath/config-local.sh" && . "$scriptsPath/config-local.sh"
 
 # Check basic required variables...
 test -f "$scriptsPath/config-check.sh" && . "$scriptsPath/config-check.sh"
@@ -16,11 +15,13 @@ test -f "$scriptsPath/config-check.sh" && . "$scriptsPath/config-check.sh"
 VERSION=`cat "$rootPath/$VERSION_FILE"`
 PROJECT_INFO=`cat "$rootPath/$PROJECT_INFO_FILE"`
 
-echo "Publishing source code $PROJECT_INFO..."
+echo "Publishing build $PROJECT_INFO..."
 
-TAG_VALUE="v.$VERSION"
-echo "Create/update tag $TAG_VALUE..." \
-  && git tag -f "$TAG_VALUE" \
-  && git push origin -f --tags \
-  && git pull origin \
-  && echo "OK"
+COMMIT_TEXT="Build $PROJECT_INFO"
+# echo "Fetch..." && git fetch && git pull && \
+cd "$PUBLISH_FOLDER" && \
+  echo "Add files..." && git add . -Av && \
+  echo "Commit..." && git commit -am "$COMMIT_TEXT" && \
+  echo "Push basic branch..." && git push && \
+  echo "Successfully published build $PROJECT_INFO" && \
+  cd ..
